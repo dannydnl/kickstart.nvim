efbc80347699f211636b9f2e4ecf11b0b9856dd5
Soldan Daniel <daniel.soldan@eberspaecher.com>
2025-01-30 18:11:45 +0200

    Changed can highlits colors

--- init.lua
+++ init.lua
@@ -92,6 +92,7 @@ vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
+vim.opt.termguicolors = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -841,22 +842,36 @@ require('lazy').setup({
       }
     end,
   },
-
-  { -- You can easily change to a different colorscheme.
+  {
+    'norcalli/nvim-colorizer.lua',
+    opts = {},
+  },
+  {
+    -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    --  'folke/tokyonight.nvim',
+    -- 'rebelot/kanagawa.nvim',
+    --  'sainnhe/gruvbox-material',
+    --   'sainnhe/everforest',
+    -- 'marko-cerovac/material.nvim',
+    -- 'olimorris/onedarkpro.nvim',
+    --  'sainnhe/sonokai',
+    'tiagovla/tokyodark.nvim',
+
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyodark'
 
       -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      vim.cmd.hi 'Comment guifg=#7ba3c9'
+      vim.cmd.hi 'Diag guifg=#dd45ff'
+      vim.cmd.hi 'Normal guifg=#d3dbe3'
     end,
   },
 
